253573940835bb6cfa951828219ea605bb7f4275
Taz Uddin <getleton@gmail.com>
2018-12-21 22:39:47 +0600

    prototype

---
+++ prototype/app.js
@@ -0,0 +1,30 @@
+var print = console.log;
+function Person() {
+    this.firstname = 'default';
+    this.lastname = 'default';
+    this.getFullname = function () {
+        return this.firstname + " " + this.lastname;
+    }
+}
+
+
+function John(fname, lname) {
+    this.firstname = fname;
+    this.lastname = lname;
+
+}
+
+
+var j1 = new John('John', 'Doe');
+j1.__proto__ = new Person();
+j2 = new John('John', 'Smith');
+j2.__proto__ = new Person();
+
+j2.getFullname = function () {
+    return 'John Jane Doe';
+}
+print(j1.getFullname());
+print(j2.getFullname());
+
+
+
